a5e48a86f782cf493f553920be6260171f5b7dd5
Juan Carlos Prieto <shox@mac.local>
2023-09-13 12:02:56 -0500

    stable version done

--- jetpack.js
+++ jetpack.js
@@ -1,20 +1,23 @@
 const getArguments = require('./lib/arguments');
 const signIn = require('./lib/signin');
 const importData = require('./lib/importData');
-const { exportData } = require('./lib/exportData'); // Importing the exportData function
+const { exportData } = require('./lib/exportData');
 const watchData = require('./lib/watchData');
 
-const argv = getArguments();
+// Use an IIFE (Immediately Invoked Function Expression) to handle async code
+(async () => {
+  const argv = await getArguments();
 
-// Main command handling logic:
-if (argv._[0] === 'signin') {
-  signIn();
-} else if (argv._[0] === 'import') {
-  importData();
-} else if (argv._[0] === 'export') {
-  exportData();
-} else if (argv._[0] === 'watch') {
-  watchData();
-} else {
-  console.error('Invalid command. For help, visit https://github.com/nodriza-io/jetpack/blob/main/README.md');
-}
+  // Main command handling logic:
+  if (argv._[0] === 'signin') {
+    signIn();
+  } else if (argv._[0] === 'import') {
+    importData(argv.domain, argv.collection, argv.format);
+  } else if (argv._[0] === 'export') {
+    exportData(argv.domain, argv.collection, argv.format);
+  } else if (argv._[0] === 'watch') {
+    watchData(argv.domain, argv.collection, argv.format);
+  } else {
+    console.error('Invalid command. For help, visit https://github.com/nodriza-io/jetpack/blob/main/README.md');
+  }
+})();
