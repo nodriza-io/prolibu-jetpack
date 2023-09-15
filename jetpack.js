09e42a7225f445e85ceb091063587d4f3f06f74a
Juan Carlos Prieto <shox@mac.local>
2023-09-15 04:40:07 -0500

    stable version

--- jetpack.js
+++ jetpack.js
@@ -1,12 +1,17 @@
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
 const getArguments = require('./lib/arguments');
 const signIn = require('./lib/signin');
+const signOut = require('./lib/signout');
 const importData = require('./lib/importData');
 const { exportData } = require('./lib/exportData');
 const watchData = require('./lib/watchData');
 const preview = require('./lib/preview');
+global.u = require('./lib/Utils');
 
 const commandHandlers = {
   'signin': signIn,
+  'signout': signOut,
   'import': (args) => importData(args.domain, args.collection, args.format),
   'export': (args) => exportData(args.domain, args.collection, args.format),
   'watch': (args) => watchData(args.domain, args.collection, args.format),
@@ -15,14 +20,15 @@ const commandHandlers = {
 
 (async () => {
   try {
-    const argv = await getArguments();
+    const yargsInstance = yargs(hideBin(process.argv));
+    const argv = await getArguments(yargsInstance);
+
     const command = argv._[0];
 
     if (commandHandlers[command]) {
       commandHandlers[command](argv);
     } else {
-      console.error('Invalid command. For help, visit https://github.com/nodriza-io/jetpack/blob/main/README.md');
-      // Optionally, you can display a help message here with available commands.
+      yargsInstance.showHelp();
     }
   } catch (error) {
     console.error(`An error occurred: ${error.message}`);
